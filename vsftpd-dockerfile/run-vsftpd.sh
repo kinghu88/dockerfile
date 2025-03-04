06653efda7578ae2f88ea1e265ec9b3520ecc81f
KingHu88 <517834203@qq.com>
2025-03-04 23:43:20 +0800

    update

--- vsftpd-dockerfile/run-vsftpd.sh
+++ vsftpd-dockerfile/run-vsftpd.sh
@@ -14,7 +14,7 @@ fi
 if [ "$LOG_STDOUT" = "**Boolean**" ]; then
     export LOG_STDOUT=''
 else
-    export LOG_STDOUT='Yes.'
+    export LOG_STDOUT='Yes'
 fi
 
 # Create home dir and update vsftpd user db:
